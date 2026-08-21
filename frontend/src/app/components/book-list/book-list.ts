3086781f6cf3b13dc3bc523ae07b93b1b1e1bd1f
yunjia <effie.ban1987@gmail.com>
2026-08-21 10:18:37 +0200

    Implement Book list page with add, edit, and delete functionality

--- frontend/src/app/components/book-list/book-list.ts
+++ frontend/src/app/components/book-list/book-list.ts
@@ -1,11 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Book, BookModel } from '../../services/book';
 
 @Component({
   selector: 'app-book-list',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './book-list.html',
   styleUrl: './book-list.css',
 })
-export class BookList {
+export class BookList implements OnInit {
+  books: BookModel[] = [];
 
+  newTitle = '';
+  newAuthor = '';
+  newPublishedDate = '';
+
+  editingId: number | null = null;
+  editTitle = '';
+  editAuthor = '';
+  editPublishedDate = '';
+
+  constructor(private bookService: Book) {}
+
+  ngOnInit() {
+    this.loadBooks();
+  }
+
+  loadBooks() {
+    this.bookService.getBooks().subscribe({
+      next: (data) => {
+        this.books = data;
+      }
+    });
+  }
+
+  addBook() {
+    const newBook = {
+      title: this.newTitle,
+      author: this.newAuthor,
+      publishedDate: this.newPublishedDate
+    };
+    this.bookService.addBook(newBook).subscribe({
+      next: () => {
+        this.newTitle = '';
+        this.newAuthor = '';
+        this.newPublishedDate = '';
+        this.loadBooks();
+      }
+    });
+  }
+
+  startEdit(book: BookModel) {
+    this.editingId = book.id;
+    this.editTitle = book.title;
+    this.editAuthor = book.author;
+    this.editPublishedDate = book.publishedDate;
+  }
+
+  cancelEdit() {
+    this.editingId = null;
+  }
+
+  saveEdit(id: number) {
+    const updatedBook = {
+      id: id,
+      title: this.editTitle,
+      author: this.editAuthor,
+      publishedDate: this.editPublishedDate
+    };
+    this.bookService.updateBook(id, updatedBook).subscribe({
+      next: () => {
+        this.editingId = null;
+        this.loadBooks();
+      }
+    });
+  }
+
+  deleteBook(id: number) {
+    this.bookService.deleteBook(id).subscribe({
+      next: () => {
+        this.loadBooks();
+      }
+    });
+  }
 }
--- frontend/src/app/services/book.ts
+++ frontend/src/app/services/book.ts
@@ -1,8 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Auth } from './auth';
+
+export interface BookModel {
+  id: number;
+  title: string;
+  author: string;
+  publishedDate: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class Book {
-  
+  private apiUrl = 'http://localhost:5101/api/books';
+
+  constructor(private http: HttpClient, private authService: Auth) {}
+
+  private getAuthHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      Authorization: `Bearer ${this.authService.getToken()}`
+    });
+  }
+
+  getBooks(): Observable<BookModel[]> {
+    return this.http.get<BookModel[]>(this.apiUrl, { headers: this.getAuthHeaders() });
+  }
+
+  addBook(book: any): Observable<BookModel> {
+    return this.http.post<BookModel>(this.apiUrl, book, { headers: this.getAuthHeaders() });
+  }
+
+  updateBook(id: number, book: any): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${id}`, book, { headers: this.getAuthHeaders() });
+  }
+
+  deleteBook(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`, { headers: this.getAuthHeaders() });
+  }
 }
