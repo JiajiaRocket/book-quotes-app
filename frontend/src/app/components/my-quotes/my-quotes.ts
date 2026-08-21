12dd8b4f2ed90940b6c0ce13669b821b98f5951e
yunjia <effie.ban1987@gmail.com>
2026-08-21 10:25:21 +0200

    Implement My Quotes page with add, edit, and delete functionality

--- frontend/src/app/components/my-quotes/my-quotes.ts
+++ frontend/src/app/components/my-quotes/my-quotes.ts
@@ -1,11 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Quote, QuoteModel } from '../../services/quote';
 
 @Component({
   selector: 'app-my-quotes',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './my-quotes.html',
   styleUrl: './my-quotes.css',
 })
-export class MyQuotes {
+export class MyQuotes implements OnInit {
+  quotes: QuoteModel[] = [];
 
+  newText = '';
+  newAuthor = '';
+
+  editingId: number | null = null;
+  editText = '';
+  editAuthor = '';
+
+  constructor(private quoteService: Quote) {}
+
+  ngOnInit() {
+    this.loadQuotes();
+  }
+
+  loadQuotes() {
+    this.quoteService.getQuotes().subscribe({
+      next: (data) => {
+        this.quotes = data;
+      }
+    });
+  }
+
+  addQuote() {
+    const newQuote = {
+      text: this.newText,
+      author: this.newAuthor
+    };
+    this.quoteService.addQuote(newQuote).subscribe({
+      next: () => {
+        this.newText = '';
+        this.newAuthor = '';
+        this.loadQuotes();
+      }
+    });
+  }
+
+  startEdit(quote: QuoteModel) {
+    this.editingId = quote.id;
+    this.editText = quote.text;
+    this.editAuthor = quote.author;
+  }
+
+  cancelEdit() {
+    this.editingId = null;
+  }
+
+  saveEdit(id: number) {
+    const updatedQuote = {
+      text: this.editText,
+      author: this.editAuthor
+    };
+    this.quoteService.updateQuote(id, updatedQuote).subscribe({
+      next: () => {
+        this.editingId = null;
+        this.loadQuotes();
+      }
+    });
+  }
+
+  deleteQuote(id: number) {
+    this.quoteService.deleteQuote(id).subscribe({
+      next: () => {
+        this.loadQuotes();
+      }
+    });
+  }
 }
--- frontend/src/app/services/quote.ts
+++ frontend/src/app/services/quote.ts
@@ -1,8 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Auth } from './auth';
+
+export interface QuoteModel {
+  id: number;
+  text: string;
+  author: string;
+  userId: number;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class Quote {
-  
+  private apiUrl = 'http://localhost:5101/api/quotes';
+
+  constructor(private http: HttpClient, private authService: Auth) {}
+
+  private getAuthHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      Authorization: `Bearer ${this.authService.getToken()}`
+    });
+  }
+
+  getQuotes(): Observable<QuoteModel[]> {
+    return this.http.get<QuoteModel[]>(this.apiUrl, { headers: this.getAuthHeaders() });
+  }
+
+  addQuote(quote: any): Observable<QuoteModel> {
+    return this.http.post<QuoteModel>(this.apiUrl, quote, { headers: this.getAuthHeaders() });
+  }
+
+  updateQuote(id: number, quote: any): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${id}`, quote, { headers: this.getAuthHeaders() });
+  }
+
+  deleteQuote(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`, { headers: this.getAuthHeaders() });
+  }
 }
